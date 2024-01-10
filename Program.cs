9a1803812623546da4f01bd3e21f9da8738082f8
Antônio Apolinário <aab2@cin.ufpe.br>
2024-01-10 15:37:39 -0300

    Projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("iPhone");
+Smartphone iphone = new Iphone("3785", "Modelo bom", "87624643909", 64);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Playstore");
+
+Console.WriteLine("\nSmartphone Nokia");
+Smartphone nokia = new Nokia("7834", "Modelo melhor", "88392135002", 256);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Free fire");
+nokia.Ligar();
